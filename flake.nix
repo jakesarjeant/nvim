da4c1cd45817ee75016be97508a18791393107ea
Jake Sarjeant <jake@sarjeant.me>
2024-06-30 20:53:22 +0200

    chore: add flake-compat

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs.nixvim.url = "github:nix-community/nixvim";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
 
   outputs = {
     self,
