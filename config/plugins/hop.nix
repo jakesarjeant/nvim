abd20a0b4ab54a3dc4cfe81ce350e8dad3b65839
Jake Sarjeant <jake@sarjeant.me>
2024-07-25 17:39:23 +0200

    feat: add multiline t, f, T and F motions

---
+++ config/plugins/hop.nix
@@ -0,0 +1,55 @@
+{
+	plugins.hop.enable = true;
+	keymaps = [
+		{
+			key = "f";
+			action.__raw = ''
+				function()
+				require'hop'.hint_char1({
+						direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+						current_line_only = false
+						})
+			end
+				'';
+			options.remap = true;
+		}
+		{
+			key = "F";
+			action.__raw = ''
+				function()
+				require'hop'.hint_char1({
+						direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+						current_line_only = false
+						})
+			end
+				'';
+			options.remap = true;
+		}
+		{
+			key = "t";
+			action.__raw = ''
+				function()
+				require'hop'.hint_char1({
+						direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+						current_line_only = false,
+						hint_offset = -1
+						})
+			end
+				'';
+			options.remap = true;
+		}
+		{
+			key = "T";
+			action.__raw = ''
+				function()
+				require'hop'.hint_char1({
+						direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+						current_line_only = false,
+						hint_offset = 1
+						})
+			end
+				'';
+			options.remap = true;
+		}
+	];
+}
