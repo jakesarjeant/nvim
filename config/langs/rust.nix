b8ffe6904e5ade7f9bba631c2457fee5d64755b4
Jake Sarjeant <jake@sarjeant.me>
2024-09-11 22:44:29 +0200

    fix: disable rust conceals

--- config/langs/rust.nix
+++ config/langs/rust.nix
@@ -1,19 +1,19 @@
 {
-	autoCmd = [
-		{
-			command = "set tabstop=2 shiftwidth=2";
-			event = ["BufEnter"];
-			pattern = ["*.rs"];
-		}
-		{
-			command = "syntax match spaces /  / conceal cchar= ";
-			event = ["BufEnter"];
-			pattern = ["*.rs"];
-		}
-		{
-			command = "set conceallevel=1 concealcursor=nvi";
-			event = ["BufEnter"];
-			pattern = ["*.rs"];
-		}
-	];
+	# autoCmd = [
+	# 	{
+	# 		command = "set tabstop=2 shiftwidth=2";
+	# 		event = ["BufEnter"];
+	# 		pattern = ["*.rs"];
+	# 	}
+	# 	{
+	# 		command = "syntax match spaces /  / conceal cchar= ";
+	# 		event = ["BufEnter"];
+	# 		pattern = ["*.rs"];
+	# 	}
+	# 	{
+	# 		command = "set conceallevel=1 concealcursor=nvi";
+	# 		event = ["BufEnter"];
+	# 		pattern = ["*.rs"];
+	# 	}
+	# ];
 }
