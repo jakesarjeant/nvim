d891d245f2e816ec8b8decb3e6eee38392ba0b5e
Jake Sarjeant <jake@sarjeant.me>
2024-09-21 21:07:37 +0200

    fix: yeah I don't know what I was thinking either

--- config/plugins/better-escape.nix
+++ config/plugins/better-escape.nix
@@ -9,50 +9,50 @@
 			hash = "sha256-Usizi6w5tLM28uxibL7vEdvC8SwFi24QFSttEfhB1oo=";
 		};
 	})];
-	extraConfigLua = ''
-		require("better_escape").setup({
-			mappings = {
-				i = {
-					n = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-					e = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-				},
-				c = {
-					n = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-					e = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-				},
-				v = {
-					n = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-					e = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-				},
-				s = {
-					n = {
-						n = "<Esc>",
-						e = "<Esc>"
-					},
-					e = {
-						n = "<Esc>",
-						e = "<Esc>"
-					}
-				},
-			}
-		})
-	'';
+	# extraConfigLua = ''
+	# 	require("better_escape").setup({
+	# 		mappings = {
+	# 			i = {
+	# 				n = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 				e = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 			},
+	# 			c = {
+	# 				n = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 				e = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 			},
+	# 			v = {
+	# 				n = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 				e = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 			},
+	# 			s = {
+	# 				n = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				},
+	# 				e = {
+	# 					n = "<Esc>",
+	# 					e = "<Esc>"
+	# 				}
+	# 			},
+	# 		}
+	# 	})
+	# '';
 }
